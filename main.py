5eaca31604aaceb1e6fdd88b9bb434bff5d50b9e
Joao <joao.damorim@gmail.com>
2020-05-29 03:50:20 -0300

    Criando um json com os dados coletados.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 #res = requests.get("https://digitalinnovation.one/blog/")
@@ -12,6 +13,7 @@ soup = BeautifulSoup(res.text, 'html.parser')
 posts = soup.find_all(class_= "bloco")
 
 all_posts = []
+
 for post in posts:
     info = post.find(class_= "texts")
     info2 = post.find(class_= "info")
@@ -26,4 +28,7 @@ for post in posts:
 
     #print(autor)
 
-print(all_posts)
+#print(all_posts)
+
+with open('posts.json', 'w') as json_file:
+    json.dump(all_posts, json_file)
