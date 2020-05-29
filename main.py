68e9b35222fb878eadac23c57cfe7ff6809e10fa
Joao <joao.damorim@gmail.com>
2020-05-29 03:33:10 -0300

    Capturando títulos, textos e autores dos posts.

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ posts = soup.find_all(class_="bloco")
 
 all_posts = []
 for post in posts:
-    print(post.find('h2').text)
+    info = post.find(class_= "texts")
+    info2 = post.find(class_= "info")
+    info3 = post.find(class_ = "entry")
+    titulo = info.h2.text
+    autor = info2.a.text
+    noticia = info3.p.text
+
+    print(autor)
 
 print(all_posts)
