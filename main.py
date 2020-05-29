90f607d365ae683285e31da1d45a630f9c0e0975
Joao <joao.damorim@gmail.com>
2020-05-29 03:43:14 -0300

    Criando um dicionário com as infos de cada post.

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ res.encoding = 'utf-8'
 
 soup = BeautifulSoup(res.text, 'html.parser')
 
-posts = soup.find_all(class_="bloco")
+posts = soup.find_all(class_= "bloco")
 
 all_posts = []
 for post in posts:
     info = post.find(class_= "texts")
     info2 = post.find(class_= "info")
-    info3 = post.find(class_ = "entry")
+    info3 = post.find(class_= "entry")
     titulo = info.h2.text
     autor = info2.a.text
     noticia = info3.p.text
+    all_posts.append({'titulo': titulo,
+                      'noticia': noticia,
+                      'autor': autor
+                      })
 
-    print(autor)
+    #print(autor)
 
 print(all_posts)
